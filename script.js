eaa9702efad36797fd7dd939956e1e780993c8f8
Samanta Barros <samanta464.barros@gmail.com>
2021-07-08 17:14:25 -0300

    Adição de capitalização as propriedades do pokemon

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ function achaPokemon() {
     pokemonAPI = parseInt(Math.random() * 878) 
     $.get('https://pokeapi.co/api/v2/pokemon/'+pokemonAPI,(dado) => { 
   
-    document.querySelector("#info1").innerText = `Nome: ${dado.name}`
-    document.querySelector("#info2").innerText = `Abilidade: ${dado.abilities[0].ability.name}`;
-    document.querySelector("#info3").innerText  = `Tipo: ${dado.types[0].type.name}`
+    document.querySelector("#info1").innerText = `nome: ${dado.name}`
+    document.querySelector("#info2").innerText = `abilidade: ${dado.abilities[0].ability.name}`;
+    document.querySelector("#info3").innerText  = `tipo: ${dado.types[0].type.name}`
     })  
 };
 
